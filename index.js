01a563693ddc7f2a55ba495c3068e19181d1212d
kelvismnd <kelvismendes@gmail.com>
2019-11-27 13:19:21 -0300

    mudanças em contato

--- index.js
+++
@@ -1,8 +0,0 @@
-var txt = document.querySelector("#txt");
-
-var btn  = document.querySelector("#btn");
-
-
-function enviar(){
-   firebase
-}
--- js/carregarImg.js
+++
@@ -1,7 +0,0 @@
-var array_imgs_mais_vendidos = ["android.jpg", "tanos.jpg", "pubg.jpg"];
-var li_mais_vendidos = document.querySelectorAll(".protudos");
-
-li_mais_vendidos[0].style.backgroundImage =
-  "url(assets/actionfigure/" + array_imgs_mais_vendidos[0] + ")";
-li_mais_vendidos[1].style.backgroundImage =
-  "url(assets/actionfigure/" + array_imgs_mais_vendidos[1] + ")";
